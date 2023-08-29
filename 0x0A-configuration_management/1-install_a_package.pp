1b25f4bc99e087d920de087448520cf0feb18e29
Wandile-cyber <ralphdlamini3@gmail.com>
2023-08-29 06:26:17 -0700

    configs

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,30 +1,10 @@
 # Install puppet-lint
-# Install puppet-lint
-package { 'python3-pip':
-  ensure => installed,
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
 
 exec { 'install-flask': 
   command => "pip3 install 'flask==2.1.0'",
   path    => ['/usr/bin/', '/usr/sbin/', '/bin'],
-}
-
-file { '/etc/puppet/modules':
-  ensure => directory, 
-}
-
-file { '/var/www/html/flask_app.py':
-  content => '
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-   return "Hello from Flask!"
-',
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
 }
