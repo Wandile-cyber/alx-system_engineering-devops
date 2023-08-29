458e83ef599670122487e1d160262b8e7edbed2d
Wandile-cyber <ralphdlamini3@gmail.com>
2023-08-29 06:21:31 -0700

    configs

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,30 @@
 # Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# Install puppet-lint
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask': 
+  command => "pip3 install 'flask==2.1.0'",
+  path    => ['/usr/bin/', '/usr/sbin/', '/bin'],
+}
+
+file { '/etc/puppet/modules':
+  ensure => directory, 
+}
+
+file { '/var/www/html/flask_app.py':
+  content => '
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+   return "Hello from Flask!"
+',
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,29 +1,5 @@
 # Install puppet-lint
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install-flask':
-  command => 'pip3 install flask',
-  path    => ['/usr/bin/', '/usr/sbin/', '/bin'],
-}
-
-file { '/etc/puppet/modules':
-  ensure => directory,
-}
-
-file { '/var/www/html/flask_app.py':
-  content => '
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-   return "Hello from Flask!"
-',
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
+exec { 'pkill':
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
