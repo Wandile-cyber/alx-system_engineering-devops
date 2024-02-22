3f8e18cdf6c46e84cd36c37a7ef30edaafc506f5
Wandile-cyber <ralphdlamini3@gmail.com>
2024-02-22 21:45:24 +0000

    readme

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,3 @@
-file { 'replace last line':
-    ensure  => present,
-    path    => '/etc/default/nginx',
-    content => 'ULIMIT="-n 4096"',
-}
-
-service { 'nginx':
-    ensure    => running,
-    subscribe => File['/etc/default/nginx']
-}
+# Puppet script
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
